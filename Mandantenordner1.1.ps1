77fca4e934132e8a12bfdcc231b3e2b0bd9942af
anl-do <82723836+anl-do@users.noreply.github.com>
2021-11-06 15:39:19 +0100

    Add files via upload

---
+++ Mandantenordner1.1.ps1
@@ -0,0 +1,95 @@
+﻿<# 
+Version: 1.1
+Author: Andreas Landry (a.landry@allaround-it.de)
+Script: Mandantenordner1.1.ps1
+
+Release notes:
+Version 1.0: Original published version.
+Version 1.1: Added different Folder structure in Push Root Folder
+
+The script is provided "AS IS" with no warranties.
+
+Requirements:
+- Windows Powershell
+- definition of the folders (Variables)
+- Existing Folders (Variables)
+- no elevation required
+
+Description:
+This Script creates subfolders in the push root folder. The subfolders are made of the mandator-no. plus 5x# and 2x# to support the Simba Importmanager.
+
+HowTo:
+- Export mandates from the Simba Client by using "Zentrale Dienste" -> "Datenverwaltung" -> "Stammdatentransfer" -> "Stammdaten exportieren (Mandantenstamm)"
+- Save the Export to file named "mandanten.csv" into a folder of your choice
+- define the same folder into the variable "$csvpath"
+- edit the CSV with Notepad:
+1. Convert to UTF8
+2. Remove invalid characters for foldernames (like ")
+- define $dip and $bup variables to match the one being used in "pushfolder.ps1"
+- new folders in the Push root folders are created according to the section "create subfolder (5#,2#)"
+- for additional subfolders according the 5# or 2# scheme, please add appropriate lines in the section "create Subfolder"
+- run the scipt just once, no need to repeat. If repeated, no existing folders get overwritten
+#>
+
+#Variables
+$csvpath = 'D:\scripts\mandanten.csv'
+$dip = 'D:\pushfolder\Dokumentenimport' # document import path
+$bip = 'D:\pushfolder\Belegeimport' # belege import path
+
+# import CSV // create subfolder $bip
+$Mandantenliste = Import-Csv -Path $csvpath -Delimiter ';' -Header 'Mandantennummer', 'Mandantenname'
+foreach ($mandant in $Mandantenliste) 
+            {
+            mkdir -Path $bip -Name ($mandant.Mandantenname+ "--" +$mandant.Mandantennummer) -ErrorAction SilentlyContinue
+            }
+Start-Sleep 2
+
+# create subfolder (5#)
+$foldernames = Get-ChildItem -Path $bip -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+foreach ($fnentry in $foldernames) 
+            {
+
+            $fullpath = $fnentry.Fullname 
+
+ # Extract the 'Mandantennummer' from path
+
+            $conr = $fullpath.Substring($fullpath.LastIndexOf('--'))
+            $conr = $conr.TrimStart("--")
+    
+ # create subfolder $bip
+
+            mkdir -Path $fullpath -Name $conr##### -ErrorAction SilentlyContinue
+            mkdir -Path $fullpath -Name $conr#B#### -ErrorAction SilentlyContinue #Barbeleg
+            mkdir -Path $fullpath -Name $conr#E#### -ErrorAction SilentlyContinue #Eingangsrechnung
+            mkdir -Path $fullpath -Name $conr#A#### -ErrorAction SilentlyContinue #Ausgangsrechnung
+            mkdir -Path $fullpath -Name $conr#K#### -ErrorAction SilentlyContinue #Kassenbeleg
+            mkdir -Path $fullpath -Name $conr#C#### -ErrorAction SilentlyContinue #Kreditkartenbeleg
+            mkdir -Path $fullpath -Name $conr#G#### -ErrorAction SilentlyContinue #EC-Beleg (G für Giro Card)
+
+            }
+			
+# import CSV // create subfolder $dip
+$Mandantenliste = Import-Csv -Path $csvpath -Delimiter ';' -Header 'Mandantennummer', 'Mandantenname'
+foreach ($mandant in $Mandantenliste) 
+            {
+            mkdir -Path $dip -Name ($mandant.Mandantenname+ "--" +$mandant.Mandantennummer) -ErrorAction SilentlyContinue
+            }
+Start-Sleep 2
+
+# create subfolder (2#)
+$foldernames = Get-ChildItem -Path $dip -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+foreach ($fnentry in $foldernames) 
+            {
+
+            $fullpath = $fnentry.Fullname 
+
+ # Extract the 'Mandantennummer' from path
+
+            $conr = $fullpath.Substring($fullpath.LastIndexOf('--'))
+            $conr = $conr.TrimStart("--")
+    
+ # create subfolder $dip
+
+            mkdir -Path $fullpath -Name $conr## -ErrorAction SilentlyContinue
+            
+            }
---
+++ Pushfolder1.2.ps1
@@ -0,0 +1,109 @@
+﻿<# 
+Version: 1.2
+Author: Andreas Landry (a.landry@allaround-it.de)
+Script: Pushfolder1.2.ps1
+
+Release notes:
+Version 1.1: Original published version.
+Version 1.2: added differntiation between 'Dokumentenimport' and 'Belegeimport'
+
+The script is provided "AS IS" with no warranties.
+
+Requirements:
+- Windows Powershell
+- definition of the folders
+- Existing Folders (Variables)
+- no elevation required
+
+Description:
+This Script renames specific files from subfolders of a source folder and adds prefixes to the files (Subfoldername). The renamed files will get moved to Target folders. A logfile is created.
+
+HowTo:
+- create and define folders (Variables)
+- add or remove file extensions appropriate to your requirements (file types)
+- Include the script into task scheduler to have it executed automatically
+- All subfolders in the defined Push root folders will be processed.
+- new folders in the Push root folders are included automatically
+#>
+
+# Variables
+$dip = 'd:\pushfolder\Dokumentenimport' # document import path
+$bip = 'd:\pushfolder\Belegeimport' # 'Belege' import path
+$tf_dip = 'D:\SBCache\Transfer\' # Target folder for 'Dokumentenimport'
+$tf_bip = 'D:\SBCache\Transfer\PDFOCR\' # Target folder for 'Belegeimport'
+$lfp = 'D:\Logs' # Logfile Path
+$filetype5 = '.pdf' # file types for 5x#
+$filetype2 = '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.jpg', '.jpeg', '.png', '.bmp' # file types for 2x#
+
+# Check folders
+Try {
+    if (-Not (Test-Path $dip)) {
+        Throw "Dokumentenimport-Ordner nicht gefunden. Bitte vor der Ausführung dieses Skripts alle notwendigen Ordner anlegen."
+    }
+}
+Catch {
+    Write-Warning -message "Ein Fehler ist aufgetreten"
+    $_
+    break
+}
+
+Try {
+    if (-Not (Test-Path $bip)) {
+        Throw "Belegeimport-Ordner nicht gefunden. Bitte vor der Ausführung dieses Skripts alle notwendigen Ordner anlegen."
+    }
+}
+Catch {
+    Write-Warning -message "Ein Fehler ist aufgetreten"
+    $_
+    break
+}
+
+Try {
+    if (-Not (Test-Path $lfp)) {
+        Throw "Logfile folder nicht gefunden. Bitte vor der Ausführung dieses Skripts alle notwendigen Ordner anlegen."
+    }
+}
+Catch {
+    Write-Warning -message "Ein Fehler ist aufgetreten"
+    $_
+    break
+}
+
+# Transcript
+#Start-Transcript -Append -Path -UseMinimalHeader $lfp\pushfolder.log
+
+# file renaming 5x#
+$foldernames = Get-ChildItem -Path $bip -Recurse -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName, name
+foreach ($row in $foldernames)
+{
+$Items = get-childitem -File -Path $row.fullname | Where-Object {$_.extension -in $filetype5}
+Write-Host $row.fullname " includes:" $Items
+$items | Rename-Item -NewName { $row.name + $_.Name } -Verbose -Force
+}
+
+# file move with filter 5x#
+$filenames = Get-ChildItem -Path $bip -Recurse -file -Force -ErrorAction SilentlyContinue | Select-Object fullname, Name
+foreach ($customer in $filenames)
+{
+$cnr = Split-Path $customer.fullname -Parent | Split-Path -Leaf
+Move-Item -Path $customer.fullname -Include $cnr* -Destination $tf_bip -verbose -force -ErrorAction SilentlyContinue
+}
+
+# file renaming 2x#
+$foldernames = Get-ChildItem -Path $dip -Recurse -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName, name
+foreach ($row in $foldernames)
+{
+$Items = get-childitem -File -Path $row.fullname | Where-Object {$_.extension -in $filetype2}
+Write-Host $row.fullname " includes:" $Items
+$items | Rename-Item -NewName { $row.name + $_.Name } -Verbose -Force
+}
+
+# file move with filter 2x#
+$filenames = Get-ChildItem -Path $dip -Recurse -file -Force -ErrorAction SilentlyContinue | Select-Object fullname, Name
+foreach ($customer in $filenames)
+{
+$cnr = Split-Path $customer.fullname -Parent | Split-Path -Leaf
+Move-Item -Path $customer.fullname -Include $cnr* -Destination $tf_dip -verbose -force -ErrorAction SilentlyContinue
+}
+
+#Stop-Transcript
